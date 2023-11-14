45b424386fc782e455ce882645e1e11308d815e7
AbdulCodeCraft <ar.abdulrahman.a@gmail.com>
2023-11-14 12:39:31 +0000

    Light Theme Adding or not checking

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite"
-
+import { resolve } from 'path'
 export default defineConfig({
-    base:"/Instagram-clone/"
+    base:"/Instagram-clone/",
+    build :{
+       rollupOption:{
+        Input:{
+            index:resolve(__dirname,'index.html'),
+            light:resolve(__dirname,'Light-Theme.html')
+        }
+       }
+    }
     
 })
